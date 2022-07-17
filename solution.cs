fd255e101f93ac13f0bd9829fc43e25bb8d49c04
Kooreis <kooreis.official@gmail.com>
2022-07-17 03:05:33 -0500

    This update adds a nested loop that iterates over each cell in the Sudoku board. If the cell is not empty, it calculates the index of the box that the cell belongs to. It then checks if the number is already present in the corresponding row, column, or box. If it is, it returns false. Otherwise, it adds the number to the corresponding sets.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,16 @@
-function isValidSudoku(board) {
-    let rows = new Array(9).fill(null).map(() => new Set());
-    let columns = new Array(9).fill(null).map(() => new Set());
-    let boxes = new Array(9).fill(null).map(() => new Set());
-}
+for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let num = board[i][j];
+            if (num !== '.') {
+                let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+                if (rows[i].has(num) || columns[j].has(num) || boxes[boxIndex].has(num)) {
+                    return false;
+                }
+
+                rows[i].add(num);
+                columns[j].add(num);
+                boxes[boxIndex].add(num);
+            }
+        }
+    }
