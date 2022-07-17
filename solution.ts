ab1ca3fb1c9ac870a646bfe3027b8deac4e53c32
Kooreis <kooreis.official@gmail.com>
2022-07-17 03:05:33 -0500

    Added methods to validate rows and columns of the Sudoku board. These methods check if any number (other than 0) is repeated in the same row or column.

--- solution.ts
+++ solution.ts
@@ -1,7 +1,25 @@
-class SudokuValidator {
-    board: number[][];
-
-    constructor(board: number[][]) {
-        this.board = board;
+isValidRow(row: number): boolean {
+        let digits = new Set<number>();
+        for (let col = 0; col < 9; col++) {
+            if (this.board[row][col] !== 0) {
+                if (digits.has(this.board[row][col])) {
+                    return false;
+                }
+                digits.add(this.board[row][col]);
+            }
+        }
+        return true;
     }
-}
+
+    isValidColumn(col: number): boolean {
+        let digits = new Set<number>();
+        for (let row = 0; row < 9; row++) {
+            if (this.board[row][col] !== 0) {
+                if (digits.has(this.board[row][col])) {
+                    return false;
+                }
+                digits.add(this.board[row][col]);
+            }
+        }
+        return true;
+    }
